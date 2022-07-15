41ecd41bff7f1dc430c9e32fc4ca409ed731ea63
cognivore <noreply>
2022-07-15 21:36:51 +0100

    Problem: Prototyping leaves mess
    Solution:
    
     - Sort out the layout of modules and their dependencies:
    
    Straume
           .Zeptoparsec -- help parse simple protocols like protobufs and such -.
                                                                                |
                                                                                |
                                                                                v
           .Chunk -- chunks that bundle Stream data into objects, parsable entirely with a Megaparsec ---.
                                                                |                                        /
                                                                |                                       /
                                                                v                                      /
           .Buffer -- extract several things out of monadic context                                   /
                                                                                                     /
           .Clock -- logical clocks, including system clock -.                                      /
                                                            /                                      /
                                                           /--------------------------------------`
           .Uni -- basic stream with just one source <----`

--- Main.lean
+++ Main.lean
@@ -1,15 +1,6 @@
 import Straume
 
-def askFrom : IO IO.FS.Handle → Nat → IO (IO.FS.Handle × ByteArray) :=
-  Straume.instMonadEmitIOHandleByteArray.askFrom
-
 def main : IO Unit := do
-  IO.println "STARTING STREAM DEMO 2"
-  let (_, zoink) <-
-    IO.FS.withFile (System.mkFilePath [".", "Straume.lean"])
-                   (.read)
-                   (fun handle => askFrom (pure handle) 228)
-  IO.println s!"{String.fromUTF8Unchecked zoink}"
+  IO.println "STREAM DEMO 3 COMING SOON"
 
 #check IO.FS.withFile
-#check Straume.MonadEmit.askFrom
