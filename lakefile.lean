c71d0e381bb0608c39765b9997075d576067efaf
Ilona Prikule <ilona@yatima.io>
2022-07-27 18:57:34 +0200

    [#1] Add some Zeptoparsec tests
    Problem: no Zeptoparsec tests.
    
    Solution: added some tests for string-specific stuff and some more
    for generic combinators.

--- lakefile.lean
+++ lakefile.lean
@@ -3,8 +3,15 @@ open Lake DSL
 
 package Straume
 
+require LSpec from git
+  "https://github.com/yatima-inc/LSpec.git" @ "3b759f6e7798fdb6b17ae83ea060cd34e89b7e91"
+
 @[defaultTarget]
 lean_exe straume {
   supportInterpreter := true
   root := "Main"
 }
+
+-- Tests
+
+lean_exe Tests.Zeptoparsec
