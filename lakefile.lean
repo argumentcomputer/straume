245f6edc14480bdc77ad06cc58b4786708362698
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-08-01 22:15:48 -0300

    bumping lspec and toolchain (#7)
    bumping lspec and toolchain

--- lakefile.lean
+++ lakefile.lean
@@ -4,9 +4,7 @@ open Lake DSL
 package Straume
 
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec.git" @ "3b759f6e7798fdb6b17ae83ea060cd34e89b7e91"
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "8f609e0ed826dde127c8bc322cb6f91c5369d37a"
+  "https://github.com/yatima-inc/LSpec.git" @ "77fc51697abeff937ffd20d2050723dc0fa1c8c0"
 
 @[defaultTarget]
 lean_exe straume {
