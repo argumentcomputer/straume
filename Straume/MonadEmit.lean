dd5a370128f4f4bc699785e12809c951e1595325
Ilona Prikule <ilona@yatima.io>
2022-08-04 01:53:28 +0200

    Add MonadEmit instances
    Problem: we only have a MonadEmit instance for ByteArray, and it's
    likely we will need strings to represent stuff for the user.
    
    Solution: added a MonatEmit instance for String, changed the askFrom
    signature dropping the wrapper type requirement from source. Also
    added a separate function readStringN to read n-length slices from
    a given handle.

--- Straume/MonadEmit.lean
+++ Straume/MonadEmit.lean
@@ -1,12 +1,19 @@
 namespace Straume.MonadEmit
 
 class MonadEmit (m : Type u → Type v) (source : Type u) (value : Type u) where
-  askFrom : m source → Nat → m (value × source)
+  askFrom : source → Nat → m (value × source)
 
 export MonadEmit (askFrom)
 
 instance : MonadEmit IO IO.FS.Handle ByteArray where
-  askFrom (src : IO IO.FS.Handle) (n : Nat) := do
-    let handler ← src
-    let bs ← IO.FS.Handle.read handler (USize.ofNat n)
-    return (bs, handler)
+  askFrom src n := do
+    let ba ← IO.FS.Handle.read src $ USize.ofNat n
+    return (ba, src)
+
+instance : MonadEmit IO IO.FS.Handle String where
+  askFrom src n := do
+    let (ba, handle) ← MonadEmit.askFrom src n
+    return (String.fromUTF8Unchecked ba, handle)
+
+def readStringN : IO.FS.Handle → Nat → IO (String × IO.FS.Handle)
+  := MonadEmit.askFrom
