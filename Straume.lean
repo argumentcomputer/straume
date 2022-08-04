467cc0d666d1a3320956981f5bedb768eee82281
cognivore <noreply>
2022-08-04 14:14:11 +0100

    Problem: No unified Stream API for Megaparsec and others to enjoy
    Solution:
    
     - Unify those things into a Straume typeclass
    
    Further work: instances.

--- Straume.lean
+++ Straume.lean
@@ -2,6 +2,7 @@ import Straume.Aka
 import Straume.Buffer
 import Straume.Chunk
 import Straume.Clock
+import Straume.Iterator
 import Straume.MonadEmit
 import Straume.Pos
 import Straume.Prob.Ordering
@@ -9,3 +10,19 @@ import Straume.Uni
 import Straume.Zeptoparsec
 
 namespace Straume
+
+open Straume.Iterator in
+open Straume.Chunk in
+open Straume.Aka in
+/- Stream with an injective type family from composite `v` to atomic `a`. -/
+class Straume (m : Type u → Type v)
+              (s : Type u)
+              (f : Type u → Type u)
+              (v : Type u)
+              (a : outParam (Type u))
+              [outParam (Terminable f)]
+              [outParam (Iterable v a)] where
+  take1 (_source : s) (_bufferSize : Nat) : m ((f a) × s)
+  takeN (_readQty : Nat) (_source : s) (_bufferSize : Nat) : m ((f v) × s)
+  takeWhile (_atomicMatcher : a → Bool) (_source : s) (_bufferSize : Nat) : m ((f v) × s)
+  chunkLength (chunk : f v) : Nat := Aka.chunkLength chunk
