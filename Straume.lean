898c72ba593d4a98e5d9d75a3fc819b0581bbfb8
cognivore <noreply>
2022-08-04 15:27:50 +0100

    fail

--- Straume.lean
+++ Straume.lean
@@ -13,29 +13,38 @@ import Straume.Zeptoparsec
 
 namespace Straume
 
-open Straume.Iterator in
-open Straume.Chunk in
-open Straume.Aka in
-open Straume.Flood in
-open Straume.Coco in
-/- Stream with an injective type family from composite `v` to atomic `a`. -/
+universe u
+universe v
+
+open Straume.Iterator
+open Straume.Chunk
+open Straume.Aka
+open Straume.Flood
+open Straume.Coco
+
+/- Stream with an bijective type family from composite `v` to atomic `a`. -/
 class Straume (m : Type u → Type v)
               (s : Type u)
               (f : Type u → Type u)
               (v : Type u)
-              (a : outParam (Type u))
               [Terminable f]
               [Flood m s]
               [Monad m]
               [Coco v s]
-              [Inhabited α]
-              [outParam (Iterable v a)] where
-  take1 (_source : s) (_bufferSize : Nat) : m (f a × s) :=
-    Aka.take1 _source _bufferSize
-  takeN (_readQty : Nat) (_source : s) (_bufferSize : Nat) : m (f v × s) :=
-    Aka.takeN _source _readQty _bufferSize
-  takeWhile (_atomicMatcher : a → Bool) (_source : s) (_bufferSize : Nat) : m (f v × s) := Aka.takeWhile _atomicMatcher _source _bufferSize
+              [Inhabited (m (f v × s))]
+              [Inhabited v]
+              [Iterable v a] where
+  take1 (source : s) (bufferSize := 2048) : m (f a × s) :=
+    Aka.take1 source bufferSize
+  takeN (readQty : Nat) (source : s) (bufferSize := 2048) : m (f v × s) :=
+    Aka.takeN readQty source bufferSize
+  takeWhile (atomicMatcher : a → Bool) (source : s) (bufferSize := 2048) : m (f v × s) :=
+    Aka.takeWhile atomicMatcher source bufferSize
   chunkLength (chunk : f v) : Nat := Aka.chunkLength chunk
+
+export Straume (take1 takeN takeWhile chunkLength)
 
 open Straume.Combinators
 #check λs => Straume.Aka.takeWhile (const true) s 2048
+
+instance : Straume IO (String × IO.FS.Handle) Chunk String := {}
