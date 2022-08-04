c87f8a7ed2adcc2d3a487688614ac786ea7ad375
Ilona Prikule <ilona@yatima.io>
2022-08-04 04:57:26 +0200

    Add examples to Main.lean
    Problem: Straume should theoretically work 🎉, but we should showcase
    how to use it, for now at least in Main.
    
    Solution: added some fun with Victorian literature to Main.lean main fn.

--- Main.lean
+++ Main.lean
@@ -1,9 +1,33 @@
 import Straume
 
+open Straume.Chunk in
+open Straume.Aka in
+
 def main : IO Unit := do
-  IO.println "STREAM DEMO 3! Now in 3D!"
-  let (zoink, _) ←
-    IO.FS.withFile (System.mkFilePath [".", "Straume.lean"])
-                   (.read)
-                   (fun handle => Straume.MonadEmit.askFrom (pure handle) 3555)
-  IO.println s!"{String.fromUTF8Unchecked zoink}"
+  IO.println "STREAM DEMO 9000! NOW WORKING!"
+  let file := System.mkFilePath ["./Tests/", "PrideAndPrejudice.txt"]
+
+  IO.println "Let's read some Jane Austin!\n"
+  let (zoink, h₀) ←
+    IO.FS.withFile file (.read)
+                (fun handle => Straume.MonadEmit.askFrom handle 38)
+  IO.println s!"{String.fromUTF8Unchecked zoink}\n"
+
+  IO.println "Nice! How does that sentence end? Let's find out, with CHUNKS!\n"
+  let ((zoink1 : Chunk String), (buff₁, h₁)) ← takeN (pure ("", h₀)) 80 200
+  IO.println s!"CHUNK IS {zoink1}"
+  IO.println s!"BUFFER IS {buff₁}"
+
+  IO.println "\nOh, it's a classic. Let's read the whole Chapter 1!\n"
+  let ((zoink2 : (Chunk String)), (buff₂, _)) ← takeN (pure ("", h₁)) 80000
+  IO.println s!"CHUNK IS {zoink2}"
+  IO.println s!"BUFFER IS {if buff₂ == "" then "EMPTY" else buff₂}"
+
+  IO.println "\nVery cool. Let's go from the top UNTIL we find a PERIOD.\n"
+  let ((zoink3 : (Chunk String)), (buff₃, _)) ←
+    IO.FS.withFile file (.read)
+                   (fun handle =>
+                      takeWhile (fun c => c != '.') (pure ("", handle)))
+
+  IO.println s!"CHUNK IS {zoink3}"
+  IO.println s!"BTW, CHUNK'S SIZE IS {chunkLength zoink3}"
--- Straume.lean
+++ Straume.lean
@@ -1,11 +1,11 @@
-import Straume.Zeptoparsec
-import Straume.Chunk
+import Straume.Aka
 import Straume.Buffer
+import Straume.Chunk
 import Straume.Clock
-import Straume.Uni
-import Straume.Clock
-import Straume.Prob.Ordering
-import Straume.Pos
 import Straume.MonadEmit
+import Straume.Pos
+import Straume.Prob.Ordering
+import Straume.Uni
+import Straume.Zeptoparsec
 
 namespace Straume
