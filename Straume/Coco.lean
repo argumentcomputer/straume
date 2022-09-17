59f6af9a3bce0d3e463b5cd00392ced438c50ef3
mpenciak <matej.penciak@gmail.com>
2022-09-17 15:57:17 -0400

    Chore: Coco docstring

--- Straume/Coco.lean
+++ Straume/Coco.lean
@@ -1,9 +1,11 @@
 namespace Straume.Coco
 
-/- `Coco`s are `y`s that carry and allow to swap out `x`s.
+/--
+`Coco`s are `y`s that carry and allow to swap out `x`s.
 So `x`s are "contained" in `y`s.
 Sort of Flipped Coe with an additional `replace` method.
-Preventing typeclass clashes in transient dependencies in style. -/
+Preventing typeclass clashes in transient dependencies in style. 
+-/
 class Coco (x : Type u) (y : outParam (Type u)) where
   coco : y → x
   replace : y → x → y
