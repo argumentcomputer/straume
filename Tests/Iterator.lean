46066d92b0dd35e6a02e48f850f0717f449af894
Ilona Prikule <ilona@yatima.io>
2022-10-24 13:37:49 +0200

    Bump YatimaStdLib dependency, group tests
    Problem: we need to address some namespace changes regarding Bit
    type in YatimaStdLib. Also, groups were introduced in LSpec ages ago,
    but we're still not using them.
    
    Solution: bumped YatimaStdLib dependency and fixed stuff broken by it.
    Also grouped Iterator tests, mostly based on the type but sometimes on
    semantics.

--- Tests/Iterator.lean
+++ Tests/Iterator.lean
@@ -6,10 +6,9 @@ import Straume.Iterator
 
 open LSpec
 open Straume.Iterator
-open ByteArray hiding toList
 
 def str : Iterator String := iter "This is a test string of 39 characters."
-def bits : Iterator (List Bit) := iter $ pad 10 $ natToBits 43
+def bits : Iterator (List Bit) := iter $ Bit.listPad 10 $ Nat.toBits 43
 def emptyBA : Iterator ByteArray := iter default
 
 -- A helper function to turn any `Iterator` to `List` and back again,
@@ -21,43 +20,42 @@ private def lrt
 def listsTest :=
   let rtstr := "There and back again"
   let rtba := List.toByteArray [42, 10, 3]
-  let rtbits := natToBits 10
-  test "toList/fromList roundtrip, strings" (fromList (toList rtstr) = rtstr) $
-  test "toList/fromList roundtrip, strings" (fromList (toList rtba) = rtba) $
-  test "toList/fromList roundtrip, strings" (fromList (toList rtbits) = rtbits)
+  let rtbits := Nat.toBits 10
+  group "toList/fromList roundtrip" $
+    test "strings" (fromList (toList rtstr) = rtstr) $
+    test "bytearray" (fromList (toList rtba) = rtba) $
+    test "bits" (fromList (toList rtbits) = rtbits)
 
 def extractTest
   (tdesc : String) (it : Iterator α)
     [Iterable α β] [Inhabited α] [DecidableEq α] : TestSeq :=
-  test (tdesc ++ ", extracting 0↔0: empty") (extract it it = default) $
-  test (tdesc ++ ", extracting 0↔4: four symbols")
-    (extract it { it with i := 4 } = lrt it.s (List.take 4)) $
-  test (tdesc ++ ", extracting 4↔8: four symbols")
-    (extract { it with i := 4 } { it with i := 8 } =
-      lrt it.s (List.take 4 ∘ List.drop 4)) $
-  test (tdesc ++ ", extracting 0↔n, n > size: returns the whole string") $
-    extract it {it with i := 100} = it.s
+  group s!"{tdesc}, extracting" $
+    test "0↔0: empty" (extract it it = default) $
+    test "0↔4: four symbols"
+      (extract it { it with i := 4 } = lrt it.s (List.take 4)) $
+    test "4↔8: four symbols"
+      (extract { it with i := 4 } { it with i := 8 } =
+        lrt it.s (List.take 4 ∘ List.drop 4)) $
+    test "0↔n, n > size: returns the whole string" $
+      extract it {it with i := 100} = it.s
 
 def forwardTest
   (tdesc : String) (it : Iterator α) [Iterable α β]
     [DecidableEq α] [DecidableEq β] [Inhabited α] [Inhabited β] : TestSeq :=
   let last := if it.s = default then default else (toList it.s).getLast!
-  test (tdesc ++ ", no forwards past the edge")
-    (curr (forward it 100) = last) $
-  test (tdesc ++ ", manual going past the edge returns last char") $
-    curr ({ it with i := 100 }) = last
-
-def singletonTest
-  (tdesc : String) (it : Iterator α)
-    [Iterable α β] [DecidableEq β] [Inhabited β] : TestSeq :=
-  test (tdesc ++ ", hasNext when only one element") $ hasNext it
+  group s!"{tdesc}, forwarding" $
+    test "no forwards past the edge"
+      (curr (forward it 100) = last) $
+    test "manual going past the edge returns last char" $
+      curr ({ it with i := 100 }) = last
 
 def emptyTest
   (tdesc : String) (it : Iterator α)
     [Iterable α β] [DecidableEq β] [Inhabited β] : TestSeq :=
-  test (tdesc ++ ", no hasNext on empty iterator") (hasNext it = false) $
-  test (tdesc ++ ", current elem of empty iterator = default") $
-    curr it = default
+  group s!"{tdesc}, empty" $
+    test "no hasNext on empty iterator" (hasNext it = false) $
+    test "current elem of empty iterator = default" $
+      curr it = default
 
 def main := lspecIO $
   listsTest ++
@@ -68,9 +66,10 @@ def main := lspecIO $
   forwardTest "String" str ++
   forwardTest "List Bit" str ++
 
-  singletonTest "String" (iter ".") ++
-  singletonTest "List Bit" (iter [Bit.one]) ++
-  singletonTest "ByteArray" (iter $ List.toByteArray [42]) ++
+  group "hasNext when only one element" (
+    test "String" (hasNext $ iter ".") $
+    test "List Bit" (hasNext $ iter [Bit.one]) $
+    test "ByteArray" (hasNext $ iter $ List.toByteArray [42])) ++
 
   emptyTest "String" (iter "") ++
   emptyTest "List Bit" ((iter []) : Iterator (List Bit)) ++
