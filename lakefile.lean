3cade31a99edcb048e182a95be0a991e41e1a0af
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-01-02 12:27:22 -0300

    bump toolchain

--- lakefile.lean
+++ lakefile.lean
@@ -6,16 +6,14 @@ package Straume
 lean_lib Straume
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "f905b68f529de2af44cf6ea63489b7e3cd090050"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "e3078e5496b3c5e635792081ea1a483b33ed536b"
 
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec.git" @ "89798a6cb76b2b29469ff752af2fd8543b3a5515"
+  "https://github.com/yatima-inc/LSpec.git" @ "129fd4ba76d5cb9abf271dc29208a28f45fd981e"
 
 @[default_target]
-lean_exe straume {
-  supportInterpreter := true
-  root := "Main"
-}
+lean_exe straume where
+  root := `Main
 
 lean_exe Tests.Iterator
 lean_exe Tests.Zeptoparsec
