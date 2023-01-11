f06aab93bf3692a98188b1810d51003237e9027e
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-01-11 10:38:37 -0300

    bump libs and toolchain

--- lakefile.lean
+++ lakefile.lean
@@ -5,11 +5,11 @@ package Straume
 
 lean_lib Straume
 
-require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "515b7eff7765dcf55ce275ac41fa13a30a59f1d0"
-
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec.git" @ "129fd4ba76d5cb9abf271dc29208a28f45fd981e"
+  "https://github.com/yatima-inc/LSpec.git" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
+
+require YatimaStdLib from git
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "704823e421b333ea9960347e305c60f654618422"
 
 @[default_target]
 lean_exe straume where
