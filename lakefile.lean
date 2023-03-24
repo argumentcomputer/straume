0722ec85222cc6cd9184d11d9bb601cad65f4572
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-03-24 11:06:38 -0300

    update deps org

--- lakefile.lean
+++ lakefile.lean
@@ -6,10 +6,10 @@ package Straume
 lean_lib Straume
 
 require LSpec from git
-    "https://github.com/yatima-inc/LSpec" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
+  "https://github.com/lurk-lab/LSpec" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "856a4e605bc55e242202459871cdcca0b4ba857e"
+  "https://github.com/lurk-lab/YatimaStdLib.lean" @ "856a4e605bc55e242202459871cdcca0b4ba857e"
 
 @[default_target]
 lean_exe straume where
